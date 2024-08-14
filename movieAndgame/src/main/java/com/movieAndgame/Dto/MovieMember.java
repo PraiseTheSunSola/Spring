1b8fd650a3bc6d8788f30853410b5b58701aa924
PraiseTheSunSola <pledgemo2115@gmail.com>
2024-08-14 14:18:22 +0900

    게임사이트에도 회원가입 양식 및 유효성검사까지 만들어봄

--- movieAndgame/src/main/java/com/movieAndgame/Dto/MovieMember.java
+++ movieAndgame/src/main/java/com/movieAndgame/Dto/MovieMember.java
@@ -1,15 +1,64 @@
 package com.movieAndgame.Dto;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
 public class MovieMember {
+	
+	@NotEmpty(message="이메일을 입력하세요")
 	private String email;
+	
+	@Size(min=4,max=12, message="비밀번호는 4~12자리로 입력하세요")
+	@Pattern(regexp="^[a-zA-Z0-9]*$",message="영어 대소문자와 숫자만 입력하세요")
 	private String password;
+	
 	private String name;
+	
 	private int birthYear;
 	private int birthMonth;
+	
+	@NotNull(message="연락처는 필수입니다.")
 	private String tel;
 }
+
+/*
+ 	
+ 	validation 데이터 유효성 검증
+ 	
+ 	Annotation 종류
+ 	
+ 	@NotEmpty : null, 빈 문자열 확인 ""; <- 이것도 문자열 포함
+ 	@NotNull  : null 확인 
+ 	@Null     : null 입력 가능
+ 	@NotBlank : null, 빈 문자열 , 공백 불가 
+ 	
+ 	@Min(value=) : value 이하의 값만 허용
+ 		@Min(value=10, message="10이하만 입력하세요")
+ 		private int num;
+ 	@Max(value= ) : value 이상 값만 허용
+ 	  @Max(value=5, message="5이상만 입력하세요")
+ 	  private int num;
+ 	@Size(min= , max=) : 길이를 제한할때 사용
+ 	 @Size(min=10, max=20 , message="10자 이상 20자이하만 입력")
+ 	 private String title;
+ 	
+ 	@Positive : 양수값만 허용
+ 	@PositiveOrZero : 양수와 0만 허용
+ 	@Negative : 음수만 허용
+ 	@NegativeOrZero : 음수와 0만 허용
+ 	
+ 	@Email : 이메일 형식검사 , 빈문자열은 패스
+ 	@Pattern(regexp=) : 정규식 검사 
+ 
+ 
+ */
+
+
--- movieAndgame/src/main/java/com/movieAndgame/control/GameController.java
+++ movieAndgame/src/main/java/com/movieAndgame/control/GameController.java
@@ -1,11 +1,16 @@
 package com.movieAndgame.control;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.movieAndgame.Dto.GameMemberDto;
+import com.movieAndgame.Dto.MovieMember;
 
 @Controller
 @RequestMapping("/game")
@@ -25,10 +30,23 @@ public class GameController {
 		return "game/member/login";
 	}
 	
-	//회원 가입 양식 페이지 요청
+	// 회원가입 페이지 요청
 	@GetMapping("/signUp")
-	public String signup(Model model) {
-		
+	public String signUp(Model model) {
+		model.addAttribute("gameMemberDto" , new GameMemberDto());
 		return "game/member/join";
+	}
+	
+	//회원가입 작성 후 요청
+	@PostMapping("/signUp")
+	public String signup(@Valid GameMemberDto gameMemberDto,
+		BindingResult bindingResult , Model model) {
+	 if(bindingResult.hasErrors()) {
+		 System.out.println("유효하지 않은 값이 입력되었다.");
+		 
+		 return "game/member/join";
+	 
+	 }
+		return "redirect/game/join";
 	}
 }
